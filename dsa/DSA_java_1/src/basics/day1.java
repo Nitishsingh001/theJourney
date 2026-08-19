ef91617cf7525465eaf33da4620c51ea8174a924
Nitish Singh <nitishsingh92057@gamil.com>
2026-08-19 23:32:43 +0530

    final submission

---
+++ dsa/DSA_java_1/src/basics/day1.java
@@ -0,0 +1,25 @@
+package basics;
+
+import java.util.Scanner;
+
+public class day1 {
+    public static void main(String[] args){
+//        System.out.println("hiidddddddddd");
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        System.out.println(age+10);
+        int a = sc.nextInt();
+        System.out.println("first num");
+        int b = sc.nextInt();
+        System.out.println("second num");
+        int c = sc.nextInt();
+        System.out.println("third num");
+        int d = a+b+c;
+        System.out.println(d);
+
+
+
+    }
+
+}
+
